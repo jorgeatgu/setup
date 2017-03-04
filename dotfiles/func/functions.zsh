c282a9e6bd82505516134c6a0f75d00d5b0170cf
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-03-04 14:28:31 +0100

    incluyendo una nueva funcion para mantener macOS

--- dotfiles/func/functions.zsh
+++ dotfiles/func/functions.zsh
@@ -265,3 +265,19 @@ function pomostart() {
   open -a terminal-notifier --args -message "A trabajar!" -title "🍅" -appIcon "/Users/jorgeatgu/Downloads/logo.png" -sound "Glass"
 
 }
+
+# Tareas de mantenimiento del Mac
+# http://www.faq-mac.com/2016/12/empieza-el-ano-creando-una-rutina-de-mantenimiento-tu-mac/
+
+function cleanMacOS() {
+
+    osascript -e '
+    display notification "Limpiando macOS 🖥" with title "Empezamos!"'
+    sudo rm -rf ~/Library/Caches/ &&
+    sudo update_dyld_shared_cache -debug &&
+    sudo update_dyld_shared_cache -force &&
+    osascript -e '
+    display notification "Reiniciando 🙋" with title "Ahora volvemos!"
+    do shell script "/bin/sleep 3"
+    tell application "System Events" to restart'
+}
