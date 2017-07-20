801f0d21996c204ff3f54f8d337a18f56ec42028
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-07-20 22:37:21 +0200

    incluyendo por separado las funciones de initCSS

---
+++ dotfiles/func/initCSS.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+mkdir $1 &&
+cd $1 &&
+mkdir css src js img &&
+curl -O "https://raw.githubusercontent.com/jorgeatgu/base/master/{.stylelintrc,.gitignore,.stylelintignore,package.json,gulpfile.js,index.html,_variables.css,styles.css}" &&
+cd src &&
+mkdir css img js &&
+cd css &&
+curl -O https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css &&
+mv normalize.css _reset.css &&
+cd ../js &&
+touch index.js &&
+cd .. &&
+cd .. &&
+git init &&
+git add . &&
+git commit -m 'estructura creada' &&
+echo -e '\e[94m\e[1mEsto va a costar un poco' &&
+npm i &&
+git commit -m 'dependencias instaladas' &&
+npm-check -u &&
+echo -e '\e[94m\e[1mEl script ha terminado. Es hora de picar código! \U0001f913\n' &&
+afplay /System/Library/Sounds/Submarine.aiff &&
+say El script ha terminado. Es hora de picar código!
---
+++ dotfiles/func/initCSSWF.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir css src js img &&
+curl -O "https://raw.githubusercontent.com/jorgeatgu/base/master/{.stylelintrc,.gitignore,.stylelintignore,package.json,gulpfile.js,index.html,_variables.css,styles.css}" &&
+cd src &&
+mkdir css img js &&
+cd css &&
+curl -O https://raw.githubusercontent.com/necolas/normalize.css/master/normalize.css &&
+mv normalize.css _reset.css &&
+cd ../js &&
+touch index.js &&
+cd .. &&
+cd .. &&
+git init &&
+git add . &&
+git commit -m 'estructura creada' &&
+echo -e '\e[94m\e[1mEsto va a costar un poco' &&
+npm i &&
+git commit -m 'dependencias instaladas' &&
+npm-check -u &&
+echo -e '\e[94m\e[1mEl script ha terminado. Es hora de picar código! \U0001f913\n' &&
+afplay /System/Library/Sounds/Submarine.aiff &&
+say El script ha terminado. Es hora de picar código!
