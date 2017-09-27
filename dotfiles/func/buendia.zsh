d3c1804084a406b6c771b5e15e8ed3c1ed3fd2b2
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-09-27 22:35:46 +0200

    corrigiendo la funcion de buen dia, sustituyendo los dias por las horas | refactor estupido en el de WifiStatus

--- dotfiles/func/buendia.zsh
+++ dotfiles/func/buendia.zsh
@@ -1,17 +1,19 @@
   #!/bin/bash
 
-  if [[ $(date +%u) -gt 5 ]] ;
-    then
+  #Comprobamos la hora, si son más de las 18 no estamos en el trabajo :)
+  if [[ $(date +%H) -gt 18 ]] ;
+      then
       echo 'No estas en el trabajo :)'
       osascript -e '
       tell application "Spark" to activate
       tell application "Opera" to activate
       '
-    else
-      osascript -e '
-        tell application "Mail" to activate
-        tell application "Opera" to activate
-        tell application "Slack" to activate
-        tell application "Sublime Text" to activate
-      '
+  else
+    osascript -e '
+    tell application "Mail" to activate
+    tell application "Opera" to activate
+    tell application "Slack" to activate
+    tell application "Sublime Text" to activate
+    '
+    open -a terminal-notifier --args -message "A trabajar!" -title "⚒" -appIcon "/Users/jorgeatgu/Downloads/logo.png" -sound "Glass"
   fi
--- dotfiles/func/wifiStatus.sh
+++ dotfiles/func/wifiStatus.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 statusWifi=`networksetup -setairportpower en0 on`
-PURPLE='\033[00;35m'
 
 #Comprobamos si el wifi esta apagado. En caso de que devuelva off lo activamos
 if [ ! "$statusWifi" ]; then
     networksetup -setairportpower en0 on
-    echo "${PURPLE} No hay internet, activamos el Wifi"
+    echo "Activamos el Wifi"
 else
-    echo "${PURPLE} Tenemos internet!"
+    echo "Tenemos internet"
 fi
