b8fa44225df9dd1e3f5820fda18c9bc909e5ff14
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-09-28 21:37:09 +0200

    dando :rainbow: a los resultados

--- dotfiles/func/wifiStatus.sh
+++ dotfiles/func/wifiStatus.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
-statusWifi=`networksetup -setairportpower en0 on`
+statusWifi='networksetup -setairportpower en0 on'
+GREEN='\033[00;32m'
+LRED='\033[01;31m'
+wifion='activado'
+wifioff='desactivado'
 
 #Comprobamos si el wifi esta apagado. En caso de que devuelva off lo activamos
 if [ ! "$statusWifi" ]; then
     networksetup -setairportpower en0 on
-    echo "Activamos el Wifi"
+    echo -e "
+
+${GREEN} El wifi esta: $wifioff
+
+    "
 else
-    echo "Tenemos internet"
+    echo -e "
+${GREEN} El wifi esta: $wifion
+"
 fi
