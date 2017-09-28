34bb3833da688e094c839763e1e38eebbea9c103
Jorge Aznar <jorgeatgu@jorgeatgu.com>
2017-09-28 21:36:41 +0200

    actualizando la funcion de buendia e incluyendo una para testear si el wifi esta activado o no esta activado

--- dotfiles/func/buendia.zsh
+++ dotfiles/func/buendia.zsh
@@ -1,19 +1,20 @@
   #!/bin/bash
 
-  #Comprobamos la hora, si son más de las 18 no estamos en el trabajo :)
-  if [[ $(date +%H) -gt 18 ]] ;
+  #Comprobamos la hora y el día. Si son menos de las 18 y es entre lunes y viernes estas en el trabajo
+  if [ $(date +%u) -lt 5 ] && [ $(date +%H) -lt 18 ]
       then
-      echo 'No estas en el trabajo :)'
+      echo 'Estas en el trabajo :|'
       osascript -e '
-      tell application "Spark" to activate
+      tell application "Mail" to activate
       tell application "Opera" to activate
+      tell application "Slack" to activate
+      tell application "Sublime Text" to activate
       '
+      open -a terminal-notifier --args -message "A trabajar!" -title "⚒" -appIcon "/Users/jorgeatgu/Downloads/logo.png" -sound "Glass"
   else
+    echo 'No estas en el trabajo :)'
     osascript -e '
-    tell application "Mail" to activate
+    tell application "Spark" to activate
     tell application "Opera" to activate
-    tell application "Slack" to activate
-    tell application "Sublime Text" to activate
     '
-    open -a terminal-notifier --args -message "A trabajar!" -title "⚒" -appIcon "/Users/jorgeatgu/Downloads/logo.png" -sound "Glass"
   fi
