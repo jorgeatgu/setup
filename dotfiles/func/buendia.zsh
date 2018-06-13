48e546fbeadd21f4d5e8a4aad8cf6f7b831ad64b
Jorge Aznar Tobajas <jorgeatgu@jorgeatgu.com>
2018-06-13 18:20:12 +0200

    cambio de trabajo cambio de función

--- dotfiles/func/buendia.zsh
+++ dotfiles/func/buendia.zsh
@@ -3,12 +3,17 @@
   #Comprobamos la hora y el día. Si son menos de las 18 y es entre lunes y viernes estas en el trabajo
   if [ $(date +%u) -lt 5 ] && [ $(date +%H) -lt 18 ]
       then
-      echo 'Estas en el trabajo :|'
+      echo 'A trabajar!'
       osascript -e '
-      tell application "Mail" to activate
       tell application "Opera" to activate
       tell application "Slack" to activate
       tell application "Sublime Text" to activate
+      tell application "Sketch" to activate
+      tell application "Numi" to activate
+      tell application "Skype" to activate
+      tell application "log" to activate
+      tell application "Bear" to activate
+      tell application "Spotify" to activate
       '
       open -a terminal-notifier --args -message "A trabajar!" -title "⚒" -appIcon "/Users/jorgeatgu/Downloads/logo.png" -sound "Glass"
   else
@@ -18,3 +23,4 @@
     tell application "Opera" to activate
     '
   fi
+
