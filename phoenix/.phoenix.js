25eb9ab2752add5b8c3925b4520b0e2f3f6071fc
Jorge Aznar <jorgeatgu@gmail.com>
2019-04-17 15:06:25 +0200

    phoenix config and wallpaper

---
+++ phoenix/.phoenix.js
@@ -0,0 +1,10 @@
+const path = '/Users/jorgeatgu/github/phoenix-padding';
+
+require( path + '/constants.js' );
+require( path + '/keys/movement.js' );
+require( path + '/keys/focus.js' );
+require( path + '/keys/info.js' );
+
+Phoenix.set ({
+  openAtLogin: true
+});
